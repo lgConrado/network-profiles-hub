d692e9a3ac0c3ebbb6031e18c57c1a471065f338
lgConrado <lgconrado.lgc@gmail.com>
2024-10-03 16:53:08 -0300

    Estruturação do projeto

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import Rotas from './routes'
+
+createRoot(document.getElementById('root')!).render(
+  <BrowserRouter>
+  <StrictMode>
+    <Rotas />
+  </StrictMode>,
+  </BrowserRouter>
+)
---
+++ src/routes/index.tsx
@@ -0,0 +1,11 @@
+import { Route, Routes } from "react-router-dom";
+
+const Rotas = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<></>}></Route>
+    </Routes>
+  );
+};
+
+export default Rotas;
